573ccc682592331639d3fdf4af4ac978ee72eab9
RSijelmass <R.Sijelmass@gmail.com>
2017-05-11 11:09:26 +0100

    Creates 2 classes, Calculator and Printer, and add method #prints_answer

--- lib/calculator.rb
+++ lib/calculator.rb
@@ -1,7 +1,15 @@
-def add(number_1, number_2)
-  number_1 + number_2
+class Calculator
+  def add(number_1, number_2)
+    number_1 + number_2
+  end
+
+  def subtract(number, number_to_subtract)
+    number - number_to_subtract
+  end
 end
 
-def subtract(number, number_to_subtract)
-  number - number_to_subtract
+class Printer
+  def prints_answer(number)
+    "Your answer is: #{ number }"
+  end
 end
